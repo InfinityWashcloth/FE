1e8785b77bd8a2ac1e05e48017bbb086df44758d
Grishy <mailgrishy@gmail.com>
2018-11-23 11:26:26 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 var socket = io();
 
+const numOfPoint = 50;
 var ctx = document.getElementById("canvas").getContext("2d");
+
+
 var myChart = new Chart(ctx, {
   type: "line",
   data: {
-    labels: [],
+    labels: new Array(numOfPoint),
     datasets: [
       {
         label: "Predicted failure",
-        data: [],
+        data: new Array(numOfPoint),
         borderWidth: 1
       }
     ]
@@ -28,13 +31,14 @@ var myChart = new Chart(ctx, {
 
 function addData(chart, label, data) {
   chart.data.labels.push(label);
+  chart.data.labels.shift();
   chart.data.datasets.forEach(dataset => {
     dataset.data.push(data);
+    dataset.data.shift();
   });
   chart.update();
 }
 
 socket.on("update", function(msg) {
-  // console.log(msg);
   addData(myChart, msg.timestamp, msg.predict);
 });
