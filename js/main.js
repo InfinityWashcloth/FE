5bf606945a355eb0ef5b12194b88cd3d0dd67ba6
Grishy <mailgrishy@gmail.com>
2018-11-24 10:49:26 -0800

    d

--- js/main.js
+++ js/main.js
@@ -5,9 +5,14 @@ var ctx = document.getElementById("canvas").getContext("2d");
 
 var ctx3 = document.getElementById("canvas3").getContext("2d");
 
-function callback() {
-    console.log(arguments);
-}
+var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+gradientStroke.addColorStop(0, '#80b6f4');
+gradientStroke.addColorStop(1, '#f49080');
+
+var gradientFill = ctx.createLinearGradient(500, 0, 100, 0);
+gradientFill.addColorStop(0, "rgba(128, 182, 244, 0.6)");
+gradientFill.addColorStop(1, "rgba(244, 144, 128, 0.6)");
+
 
 var myChart = new Chart(ctx, {
     type: "line",
@@ -15,6 +20,18 @@ var myChart = new Chart(ctx, {
         labels: new Array(numOfPoint),
         datasets: [{
             label: "Predicted failure",
+            borderColor: gradientStroke,
+            pointBorderColor: gradientStroke,
+            pointBackgroundColor: gradientStroke,
+            pointHoverBackgroundColor: gradientStroke,
+            pointHoverBorderColor: gradientStroke,
+            pointBorderWidth: 10,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 1,
+            pointRadius: 3,
+            fill: true,
+            backgroundColor: gradientFill,
+            borderWidth: 4,
             data: new Array(numOfPoint),
             borderWidth: 1
         }]
@@ -26,25 +43,31 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold",
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    padding: 20
+                },
+                gridLines: {
+                    drawTicks: false,
+                    display: false
                 }
+
             }]
         }
     }
 });
 
-var myChart3 = new Chart(ctx3,{
+var myChart3 = new Chart(ctx3, {
     type: 'pie',
     data: {
         datasets: [{
             data: [40, 60],
             backgroundColor: [
-                "red", "green"
+                "rgba(255,0,0,0.8)", "rgba(0,255,0,0.5)"
             ]
         }],
-        
-    
-        // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [
             'Failure',
             'OK',
@@ -63,9 +86,18 @@ function addData(chart, label, data) {
     chart.update();
 }
 
+function addDataR(chart, p) {
+    chart.data.datasets.forEach(dataset => {
+        dataset.data[0] = p;
+        dataset.data[1] = 1 - p;
+    });
+    chart.update();
+}
+
 socket.on("update", function(msg) {
     for (let a of msg.amplitude) {
         addData(myChart, msg.ts, a);
     }
-    
+
+    addDataR(myChart3, msg.analysis_data)
 });
