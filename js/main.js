98e1f37d4ff9c30a5bc1b7b17ceac134c081f5c2
Grishy <mailgrishy@gmail.com>
2018-11-24 01:28:55 -0800

    push me

--- js/main.js
+++ js/main.js
@@ -5,40 +5,36 @@ var ctx = document.getElementById("canvas").getContext("2d");
 
 
 var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: new Array(numOfPoint),
-    datasets: [
-      {
-        label: "Predicted failure",
-        data: new Array(numOfPoint),
-        borderWidth: 1
-      }
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true
-          }
+    type: "line",
+    data: {
+        labels: new Array(numOfPoint),
+        datasets: [{
+            label: "Predicted failure",
+            data: new Array(numOfPoint),
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
         }
-      ]
     }
-  }
 });
 
 function addData(chart, label, data) {
-  chart.data.labels.push(label);
-  chart.data.labels.shift();
-  chart.data.datasets.forEach(dataset => {
-    dataset.data.push(data);
-    dataset.data.shift();
-  });
-  chart.update();
+    chart.data.labels.push(label);
+    chart.data.labels.shift();
+    chart.data.datasets.forEach(dataset => {
+        dataset.data.push(data);
+        dataset.data.shift();
+    });
+    chart.update();
 }
 
 socket.on("update", function(msg) {
-  addData(myChart, msg.timestamp, msg.predict);
+    addData(myChart, msg.timestamp, msg.predict);
 });
